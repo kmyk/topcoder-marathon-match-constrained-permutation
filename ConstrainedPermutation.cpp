7e5b58b01b725ed09bb46cfe5dbc0481d9a74de3
Kimiyuki Onaka <kimiyuki95@gmail.com>
2017-09-22 03:51:01 +0900

    dintinct緩和して1点飛ばす感じの近傍 微増

--- ConstrainedPermutation.cpp
+++ ConstrainedPermutation.cpp
@@ -29,6 +29,30 @@ template <class T> inline void setmin(T & a, T const & b) { a = min(a, b); }
 
 class ConstrainedPermutation { public: vector<int> permute(int N, vector<string> constraints); };
 
+template <typename T>
+map<T, int> coordinate_compression_map(vector<T> const & xs) {
+    int n = xs.size();
+    vector<int> ys(n);
+    iota(whole(ys), 0);
+    sort(whole(ys), [&](int i, int j) { return xs[i] < xs[j]; });
+    map<T,int> f;
+    for (int i : ys) {
+        if (not f.count(xs[i])) { // make unique
+            int j = f.size();
+            f[xs[i]] = j; // f[xs[i]] has a side effect, increasing the f.size()
+        }
+    }
+    return f;
+}
+
+template <typename T>
+vector<int> apply_compression(map<T,int> const & f, vector<T> const & xs) {
+    int n = xs.size();
+    vector<int> ys(n);
+    repeat (i,n) ys[i] = f.at(xs[i]);
+    return ys;
+}
+
 constexpr double ticks_per_sec = 2800000000;
 constexpr double ticks_per_sec_inv = 1.0 / ticks_per_sec;
 inline double rdtsc() { // in seconds
@@ -40,7 +64,7 @@ constexpr int TLE = 10; // sec
 
 default_random_engine gen;
 
-int compute_satisfied_of(int i, vector<int> const & p, vector<pair<int, int> > const & constraints) {
+int compute_satisfied_of(int i, vector<unsigned> const & p, vector<pair<int, int> > const & constraints) {
     int n = p.size();
     int satisfied = 0;
     repeat (j, n) {
@@ -48,7 +72,7 @@ int compute_satisfied_of(int i, vector<int> const & p, vector<pair<int, int> > c
     }
     return satisfied;
 }
-int compute_satisfied(vector<int> const & p, vector<pair<int, int> > const & constraints) {
+int compute_satisfied(vector<unsigned> const & p, vector<pair<int, int> > const & constraints) {
     int n = p.size();
     int satisfied = 0;
     repeat (i, n) {
@@ -57,28 +81,19 @@ int compute_satisfied(vector<int> const & p, vector<pair<int, int> > const & con
     assert (satisfied % 2 == 0);
     return satisfied / 2;
 }
-int compute_satisfied_of(int i, vector<int> const & p, vector<vector<int> > const & lt, vector<vector<int> > const & gt) {
+int compute_satisfied_of(int i, vector<unsigned> const & p, vector<vector<int> > const & lt, vector<vector<int> > const & gt) {
     int satisfied = 0;
     for (int j : lt[i]) satisfied += p[i] < p[j];
     for (int j : gt[i]) satisfied += p[j] < p[i];
     return satisfied;
 }
 
-double compute_score(vector<int> const & p, vector<pair<int, int> > const & constraints) {
+double compute_score(vector<unsigned> const & p, vector<pair<int, int> > const & constraints) {
     int k = constraints.size();
     return compute_satisfied(p, constraints) /(double) k;
 }
 
-vector<int> ConstrainedPermutation::permute(int n, vector<string> constraints_strings) {
-    // input
-    int k = constraints_strings.size();
-    vector<pair<int, int> > constraints(k);
-    repeat (i, k) {
-        istringstream iss(constraints_strings[i]);
-        iss >> constraints[i].first >> constraints[i].second;
-    }
-    sort(whole(constraints));
-
+vector<unsigned> permute(int n, vector<pair<int, int> > const & constraints) {
     // make graphs
     vector<vector<int> > lt(n), gt(n);
     for (auto constraint : constraints) {
@@ -91,12 +106,20 @@ vector<int> ConstrainedPermutation::permute(int n, vector<string> constraints_st
         sort(whole(gt[i]));
     }
 
-    // solve
-    vector<int> p(n);
-    iota(whole(p), 0);
+    // init
+    set<int> used;
+    vector<unsigned> p(n);
+    repeat (i, n) {
+        while (used.count(p[i])) {
+            p[i] = uniform_int_distribution<unsigned>()(gen);
+        }
+        used.insert(p[i]);
+    }
     int satisfied = compute_satisfied(p, constraints);
+
+    // simulated annealing
     int best_satisfied = satisfied;
-    vector<int> result = p;
+    vector<unsigned> result = p;
     double clock_begin = rdtsc();
     double t = 0;
     double temp = INFINITY;
@@ -112,29 +135,30 @@ cerr << "elapsed: " << t * TLE << endl;
             }
             temp = (1 - t);
         }
-        int x = uniform_int_distribution<int>(0, n - 1)(gen);
-        int y = uniform_int_distribution<int>(0, n - 1)(gen);
-        if (x > y) swap(x, y);
+        int i = uniform_int_distribution<int>(0, n - 1)(gen);
+        int p_i = p[i];
+        while (used.count(p_i)) {
+            p_i = uniform_int_distribution<unsigned>()(gen);
+        }
         int delta = 0;
-        delta -= compute_satisfied_of(x, p, lt, gt);
-        delta -= compute_satisfied_of(y, p, lt, gt);
-        delta += binary_search(whole(constraints), p[x] < p[y] ? make_pair(x, y) : make_pair(y, x));
-        swap(p[x], p[y]);
-        delta += compute_satisfied_of(x, p, lt, gt);
-        delta += compute_satisfied_of(y, p, lt, gt);
-        delta -= binary_search(whole(constraints), p[x] < p[y] ? make_pair(x, y) : make_pair(y, x));
+        delta -= compute_satisfied_of(i, p, lt, gt);
+        int previous_p_i = p[i];
+        p[i] = p_i;
+        delta += compute_satisfied_of(i, p, lt, gt);
 // assert (satisfied + delta == compute_satisfied(p, constraints));
         if (delta >= 0 or bernoulli_distribution(exp(delta / temp))(gen)) {
 // if (delta < 0) cerr << "prob: " << exp(delta / temp) << endl;
 forced += (delta < 0);
             satisfied += delta;
+            used.erase(previous_p_i);
+            used.insert(p[i]);
             if (best_satisfied < satisfied) {
                 best_satisfied = satisfied;
                 result = p;
 // cerr << "score: " << satisfied /(double) k << endl;
             }
         } else {
-            swap(p[x], p[y]);
+            p[i] = previous_p_i;
         }
     }
 // assert (rdtsc() - clock_begin < TLE);
@@ -143,4 +167,22 @@ forced += (delta < 0);
 // cerr << "p: "; for (int p_i : p) cerr << p_i << ' '; cerr << endl;
 cerr << "score: " << compute_score(result, constraints) << endl;
     return result;
+}
+
+vector<int> ConstrainedPermutation::permute(int n, vector<string> constraints_strings) {
+    // input
+    int k = constraints_strings.size();
+    vector<pair<int, int> > constraints(k);
+    repeat (i, k) {
+        istringstream iss(constraints_strings[i]);
+        iss >> constraints[i].first >> constraints[i].second;
+    }
+    sort(whole(constraints));
+
+    // solve
+    vector<unsigned> relaxed = ::permute(n, constraints);
+
+    // output
+    vector<int> p = apply_compression(coordinate_compression_map(relaxed), relaxed);
+    return p;
 }
